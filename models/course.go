beddacac68328920e1c5a105e10dfa28ca04cbd9
moroukian <moroukianali@gmail.com>
2022-02-16 16:42:53 +0330

    Add CourseGroup models

--- models/course.go
+++ models/course.go
@@ -1,6 +1,9 @@
 package models
 
-import "gorm.io/gorm"
+import (
+	"gorm.io/gorm"
+	"time"
+)
 
 type Course struct {
 	gorm.Model
@@ -12,4 +15,25 @@ type Course struct {
 	School        School
 	Prerequisites []Course `gorm:"many2many:course_prerequisites;"`
 	Corequisites  []Course `gorm:"many2many:course_corequisites;"`
+}
+
+type CourseGroup struct {
+	gorm.Model
+	ProfessorId int
+	Professor   Professor
+	CourseId    int
+	Course      Course
+	GroupNumber int `gorm:"not null"`
+	Capacity    int `gorm:"not null"`
+	ExamDate    time.Time
+	Detail      string
+}
+
+type Schedule struct {
+	gorm.Model
+	Start         time.Time `gorm:"not null"`
+	End           time.Time `gorm:"not null"`
+	Day           int       `gorm:"not null"`
+	CourseGroupId int
+	CourseGroup   CourseGroup
 }
