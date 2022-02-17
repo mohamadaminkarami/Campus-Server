5366b1974bc7547abb7bc654db7c32fa30e33441
moroukian <moroukianali@gmail.com>
2022-02-17 22:10:58 +0330

    refactor(controllers): Remove unused functions

--- controllers/school.go
+++ controllers/school.go
@@ -7,17 +7,6 @@ import (
 	"net/http"
 )
 
-func CreateSchool(c *gin.Context) {
-	var School models.School
-	if err := c.ShouldBindJSON(&School); err != nil {
-		c.JSON(http.StatusUnprocessableEntity, gin.H{"error": err})
-		return
-	}
-
-	models.GetDB().Create(&School)
-	c.JSON(http.StatusOK, serializers.SchoolToJSON(School))
-}
-
 func GetAllSchoolCourses(c *gin.Context) {
 	var schools []models.School
 	models.GetDB().Find(&schools)
@@ -38,38 +27,4 @@ func GetAllSchools(c *gin.Context) {
 		response = append(response, serializers.SchoolToJSON(u))
 	}
 	c.JSON(http.StatusOK, gin.H{"result": response})
-}
-
-func UpdateSchool(c *gin.Context) {
-	var newSchool models.School
-	var school models.School
-	if err := c.ShouldBindJSON(&newSchool); err != nil {
-		c.JSON(http.StatusUnprocessableEntity, gin.H{"message": "Bad Parameter"})
-		return
-	}
-
-	schoolId := c.Param("id")
-	object := models.GetDB().First(&school, schoolId)
-
-	if object.Error != nil {
-		c.JSON(http.StatusNotFound, gin.H{"error": "Item not found"})
-	} else {
-		if newSchool.Name != "" {
-			object.Update("Name", newSchool.Name)
-		}
-		c.JSON(http.StatusOK, serializers.SchoolToJSON(school))
-	}
-}
-
-func DeleteSchool(c *gin.Context) {
-	schoolId := c.Param("id")
-	var school models.School
-	object := models.GetDB().First(&school, schoolId)
-
-	if object.Error != nil {
-		c.JSON(http.StatusNotFound, gin.H{"error": "Item not found"})
-	} else {
-		models.GetDB().Delete(&school, schoolId)
-		c.JSON(http.StatusOK, gin.H{"message": "Item deleted"})
-	}
 }
